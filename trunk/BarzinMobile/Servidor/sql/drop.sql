29d37862660afc279139ba63e953690ee7ce79c6
laertexavier@gmail.com <laertexavier@gmail.com@5a7f6a3e-09ad-618c-bd00-81f1cd0b4a22>
2012-12-15 13:05:24 +0000

    Scripts SQL (create, delete e drop)

---
+++ trunk/BarzinMobile/Servidor/sql/drop.sql
@@ -0,0 +1,19 @@
+DROP TABLE pedidos_pessoas;
+
+DROP TABLE pessoas;
+
+DROP TABLE pedidos;
+
+DROP TABLE contas;
+
+DROP TABLE mesas;
+
+DROP TABLE itens;
+
+DROP TABLE categorias;
+
+DROP TABLE cardapios;
+
+DROP TABLE bares;
+
+DROP TABLE usuarios;
