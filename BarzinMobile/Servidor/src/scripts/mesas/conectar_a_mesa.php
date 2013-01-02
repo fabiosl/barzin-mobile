fec0ea6590cd812ea652b4ec749d7dcb18258717
joaop.ribs@gmail.com <joaop.ribs@gmail.com@5a7f6a3e-09ad-618c-bd00-81f1cd0b4a22>
2013-01-02 13:53:51 +0000

    Adicionei mais informações no retorno do conectar_a_mesa.php

--- BarzinMobile/Servidor/src/scripts/mesas/conectar_a_mesa.php
+++ BarzinMobile/Servidor/src/scripts/mesas/conectar_a_mesa.php
@@ -8,7 +8,23 @@ $banco = new DAO();
 $codigo_mesa = $_REQUEST["codigo_mesa"];
 $id_bar = $_REQUEST["id_bar"];
 
-$resultado = $banco->recupera_mesa_pelo_codigo_e_bar($codigo_mesa, $id_bar);
+$mesa = $banco->recupera_mesa_pelo_codigo_e_bar($codigo_mesa, $id_bar);
+if (get_class($mesa) == "Erro") {
+	echo $mesa->get_json();
+	exit;
+}
 
-echo $resultado->get_json();
+$bar = $banco->recupera_bar($id_bar);
+if (get_class($bar) == "Erro") {
+	echo $bar->get_json();
+	exit;
+}
+
+$conta = $banco->recupera_conta_aberta($mesa->get_id());
+if (get_class($conta) == "Erro") {
+	echo $conta->get_json();
+	exit;
+}
+
+echo "{\"mesa\": ".$mesa->get_json().", \"bar\": ".$bar->get_json().", \"conta\": ".$conta->get_json()."}";
 ?>
