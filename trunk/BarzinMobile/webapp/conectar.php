eaee2b538c689c8771143813a3ede106afc5e158
joaop.ribs@gmail.com <joaop.ribs@gmail.com@5a7f6a3e-09ad-618c-bd00-81f1cd0b4a22>
2013-01-21 00:37:07 +0000

    Revertendo uma mudançazinha que tinha colocado só pra testar o SVN do mac

--- trunk/BarzinMobile/webapp/conectar.php
+++ trunk/BarzinMobile/webapp/conectar.php
@@ -2,8 +2,6 @@
 	include_once "classes/requisicoes.php";
     include_once "classes/sessao.php";
 
-    // JOAO PAULO
-
     if (Sessao::tem("codigo_mesa") && Sessao::tem("id_bar")) {
         header('Location: index.php');
         exit;
